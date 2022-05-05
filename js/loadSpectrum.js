8b127e1f3fa3fdbd92b0c75239367b406229df12
colin-combe <colin.combe@ed.ac.uk>
2022-05-05 16:13:09 +0100

    xisearch2 wip

--- js/loadSpectrum.js
+++ js/loadSpectrum.js
@@ -10,47 +10,47 @@ export const loadSpectrum = function (match, randId) {
         formatted_data.sequence2 = match.matchedPeptides[1].seq_mods;
         formatted_data.linkPos2 = match.linkPos2 - 1;
     }
-    formatted_data.crossLinkerModMass = match.crosslinkerModMass();
-    formatted_data.modifications = xiSPEC.activeSpectrum.models.Spectrum.knownModifications;
+    // formatted_data.crossLinkerModMass = match.crosslinkerModMass();
+    // formatted_data.modifications = xiSPEC.activeSpectrum.models.Spectrum.knownModifications;
     formatted_data.precursorCharge = match.precursorCharge;
     formatted_data.fragmentTolerance = match.fragmentTolerance();
 
-    const search = window.compositeModelInst.get("clmsModel").get("searches").get(match.searchId);
-    formatted_data.customConfig = search.customsettings.split("\n");
+    formatted_data.searchConfig = window.compositeModelInst.get("clmsModel").get("searches").get(match.searchId).config;
+    // formatted_data.customConfig = search.customsettings.split("\n");
 
-    for (let cl of search.crosslinkers) {
-        formatted_data.customConfig.push(cl.description);
-    }
+    // for (let cl of search.crosslinkers) {
+    //     formatted_data.customConfig.push(cl.description);
+    // }
 
-    formatted_data.losses = [];
-    search.losses.forEach(function (loss) {
-        const formatted_loss = {};
-        const match = /(?=.*NAME:([^;]+))(?=.*aminoacids:([^;]+))(?=.*MASS:([^;]+)).*/.exec(loss.description);
-        if (match) {
-            formatted_loss.id = match[1];
-            formatted_loss.specificity = match[2].split(",");
-            formatted_loss.mass = parseFloat(match[3]);
-            if (loss.description.indexOf(";nterm") !== -1)
-                formatted_loss.specificity.push("NTerm");
-            if (loss.description.indexOf(";cterm") !== -1)
-                formatted_loss.specificity.push("CTerm");
-        }
-        formatted_data.losses.push(formatted_loss);
-        // ToDo: remove tmp fix for losses to customConfig
-        // formatted_data.customConfig.push(loss.description);
-    });
+    // formatted_data.losses = [];
+    // search.losses.forEach(function (loss) {
+    //     const formatted_loss = {};
+    //     const match = /(?=.*NAME:([^;]+))(?=.*aminoacids:([^;]+))(?=.*MASS:([^;]+)).*/.exec(loss.description);
+    //     if (match) {
+    //         formatted_loss.id = match[1];
+    //         formatted_loss.specificity = match[2].split(",");
+    //         formatted_loss.mass = parseFloat(match[3]);
+    //         if (loss.description.indexOf(";nterm") !== -1)
+    //             formatted_loss.specificity.push("NTerm");
+    //         if (loss.description.indexOf(";cterm") !== -1)
+    //             formatted_loss.specificity.push("CTerm");
+    //     }
+    //     formatted_data.losses.push(formatted_loss);
+    //     // ToDo: remove tmp fix for losses to customConfig
+    //     // formatted_data.customConfig.push(loss.description);
+    // });
 
-    const ions = match.ionTypes();
-    formatted_data.ionTypes = ions.map(function (ion) {
-        return ion.type.replace("Ion", "");
-    }).join(";");
+    // const ions = match.ionTypes();
+    // formatted_data.ionTypes = ions.map(function (ion) {
+    //     return ion.type.replace("Ion", "");
+    // }).join(";");
     formatted_data.precursorMZ = match.expMZ();
     formatted_data.requestID = match.id;
     formatted_data.spectrum_title = "PSMID: " + match.id;
 
     console.log("loadSpectrum match:" + match.id);
 
-    d3.text("../CLMS-model/php/peakList.php?sid=" + match.searchId + "-" + randId + "&spid=" + match.spectrumId, function (error, text) {
+    d3.text(window.peakListUrl + "?uuid="  + match.spectrumId, function (error, text) {
         if (error) {
             console.log("error getting peak list", error);
         } else {
@@ -61,9 +61,18 @@ export const loadSpectrum = function (match, randId) {
                 // window.xiSPEC.setData({});
             } else {
                 d3.select("#range-error").text("");
-                formatted_data.peakList = JSON.parse(text).map(function (p) {
-                    return [p.mz, p.intensity];
-                });
+                const rawPeaks = JSON.parse(text);
+                const intensity = rawPeaks[0];
+                const mz = rawPeaks[1];
+                const peakList = [];
+                const peakCount = intensity.length;
+                for (let i = 0; i < peakCount; i++){
+                    peakList.push([intensity[i], mz[i]]);
+                }
+                formatted_data.peakList = peakList;
+                // = JSON.parse(text).map(function (p) {
+                //     return [p.mz, p.intensity];
+                // });
                 console.log(formatted_data);
                 window.xiSPEC.setData(formatted_data);
             }
