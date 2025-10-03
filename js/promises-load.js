33e377ab3f4ef2930dd3fae800d641a6505ff7cd
colin combe <colin.combe@ed.ac.uk>
2025-10-03 11:21:59 +0200

    need tests import in promises-load

--- js/promises-load.js
+++ js/promises-load.js
@@ -18,6 +18,8 @@ import {displayError} from "./utils";
 import Split from "split.js";
 import {SearchResultsModel} from "../../CLMS-model/src/search-results-model";
 import {BlosumCollection} from "./model/models";
+import {testSetupNew} from "../tests/tests";
+import {testSetup2} from "../tests/tests2";
 
 export const networkPageSpinner = new Spinner({
     length: 38, // The length of each line
