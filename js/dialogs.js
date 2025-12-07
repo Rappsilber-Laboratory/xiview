e2e5c963bd7a730be513f802041f0375596b7969
colin combe <colin.combe@ed.ac.uk>
2025-12-07 11:16:00 +0100

    add JSDoc to js/dialogs.js

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview jQuery UI dialog utilities for xiVIEW.
+ * Provides convenience wrappers for creating error dialogs and confirmation dialogs.
+ * Uses jQuery UI dialog widget with modal behavior.
+ */
 import * as $ from "jquery";
 
 window.jQuery = $;
@@ -9,6 +14,15 @@ import "jquery-ui/themes/base/all.css";
 
 //todo - this wrapping object is unnecessary
 export const jqdialogs = {
+    /**
+     * Creates or updates DOM element for dialog with message paragraphs.
+     * Splits message on &lt;br&gt; tags and creates paragraph elements.
+     * Reuses existing dialog div if present, otherwise creates new one in body.
+     * @param {string} dialogID - DOM element ID for dialog container
+     * @param {string} msg - Message text (supports &lt;br&gt; for line breaks, HTML for formatting)
+     * @param {string} title - Dialog title text
+     * @returns {undefined}
+     */
     constructDialogMessage: function (dialogID, msg, title) {
         let dialogParas = d3.select("body").select("#" + dialogID);
         if (dialogParas.empty()) {
@@ -27,6 +41,15 @@ export const jqdialogs = {
             });
     },
 
+    /**
+     * Displays modal error dialog with message and GitHub link.
+     * Appends Rappsilber Lab GitHub link to message automatically.
+     * Uses jQuery UI modal dialog with default "OK" button behavior.
+     * @param {string} dialogID - DOM element ID for dialog container
+     * @param {string} msg - Error message text
+     * @param {string} [title="Database Error"] - Dialog title
+     * @returns {undefined}
+     */
     errorDialog: function (dialogID, msg, title) {
         msg = msg.concat("<br><A href='https://github.com/Rappsilber-Laboratory/' target='_blank'>Rappsilber Lab GitHub</A>");
         jqdialogs.constructDialogMessage(dialogID, msg, title || "Database Error");
@@ -36,6 +59,18 @@ export const jqdialogs = {
         });
     },
 
+    /**
+     * Displays modal confirmation dialog with customizable Yes/No buttons.
+     * Executes callback function on "Yes", closes without action on "No".
+     * Dialog is destroyed and removed from DOM after closing.
+     * @param {string} dialogID - DOM element ID for dialog container
+     * @param {string} msg - Confirmation message text
+     * @param {string} [title="Confirm"] - Dialog title
+     * @param {string} yesText - Text for "Yes" button
+     * @param {string} noText - Text for "No" button
+     * @param {Function} yesFunc - Callback function to execute if user clicks "Yes"
+     * @returns {undefined}
+     */
     areYouSureDialog: function (dialogID, msg, title, yesText, noText, yesFunc) {
         jqdialogs.constructDialogMessage(dialogID, msg, title || "Confirm");
 
