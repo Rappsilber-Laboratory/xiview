821b51cd8ea63a0e0c598f1b3cde5f5027dfc555
colin combe <colin.combe@ed.ac.uk>
2026-05-20 05:57:18 +0200

    fix string lookup

--- js/file-choosers/stringUtils.js
+++ js/file-choosers/stringUtils.js
@@ -33,8 +33,8 @@ export const STRINGUtils = {
         let rows = d3.tsv.parse(networkTsvString, function (d) {
             d.SeqPos1 = null;
             d.SeqPos2 = null;
-            d.Protein1 = stringToUniprotIDMap[d.ncbiTaxonId + "." + d.stringId_A];
-            d.Protein2 = stringToUniprotIDMap[d.ncbiTaxonId + "." + d.stringId_B];
+            d.Protein1 = stringToUniprotIDMap[d.stringId_A];
+            d.Protein2 = stringToUniprotIDMap[d.stringId_B];
             // return empty string if protein ids not in current id map
             return (d.Protein1 && d.Protein2 ? _.omit(d, ["ncbiTaxonId", "stringId_A", "stringId_B", "preferredName_A", "preferredName_B"]) : null);
         });
@@ -59,7 +59,7 @@ export const STRINGUtils = {
             const pidString = todo.join("%0d");
             const promiseObj = new Promise(function (resolve, reject) {
                 $.ajax({
-                    type: "post",
+                    type: "get",
                     url: "https://version-11-0.string-db.org/api/json/get_string_ids",
                     data: {
                         identifiers: pidString,
@@ -126,7 +126,7 @@ export const STRINGUtils = {
                 }
                 const promiseObj = new Promise(function (resolve, reject) {
                     $.ajax({
-                        type: "post",
+                        type: "get",
                         url: "https://version-11-0.string-db.org/api/tsv/network",
                         data: {
                             identifiers: networkKey,
